cb155272c578d8effeb9120653f973a12a216e94
Kerri Crawford <kerriannercrawford+github@gmail.com>
2022-05-05 17:23:40 -0400

    Fixed passthrough bug

--- spec/transform.spec.js
+++ spec/transform.spec.js
@@ -9,9 +9,11 @@ const logger = {
   error: () => {},
 };
 
+const emit = () => {};
+
 describe('Transformation test', () => {
   it('should handle simple transforms', () => {
-    return transform.process.call({ logger }, {
+    return transform.process.call({ logger, emit }, {
       data: {
         first: 'Renat',
         last: 'Zubairov',
@@ -28,7 +30,7 @@ describe('Transformation test', () => {
   });
 
   it('should not produce an empty message if transformation returns undefined', () => {
-    return transform.process.call({ logger }, {
+    return transform.process.call({ logger, emit }, {
       data: {
         first: 'Renat',
         last: 'Zubairov',
@@ -57,7 +59,7 @@ describe('Transformation test', () => {
         },
       },
     };
-    return transform.process.call({ logger }, msg, {
+    return transform.process.call({ logger, emit }, msg, {
       expression: '{ "fullName": data.first & " " & metadata.passthrough.stepA.abc}',
     }).then((result) => {
       expect(result.data).to.deep.equal({
@@ -79,7 +81,7 @@ describe('Transformation test', () => {
       var1: 'value1',
       var2: 'value2',
     };
-    return transform.process.call({ logger, getFlowVariables: () => flowVariables }, msg, {
+    return transform.process.call({ logger, emit, getFlowVariables: () => flowVariables }, msg, {
       expression: '$getFlowVariables()',
     }).then((result) => {
       expect(result.data).to.deep.equal(flowVariables);
